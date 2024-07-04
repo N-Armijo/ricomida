c73b8ade4ee611651817ab5323192f985d54cd78
narmijo-tj <toffee.jaguargmail.com>
2024-07-04 03:15:51 -0400

    Se agrega efecto smooth scroll y se mejora la responsividad. Elementos con js solicitados funcionando.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,24 @@ $(() => {
     $("#enviarCorreo").on('click', (e) =>{
         alert("El correo fue enviado correctamente");
     })
+    // cambia rojo al hacer click sobre los titulos receta y preparacion.Uno a la vez
     $("h3").on('dblclick', (event) => {
         $(event.currentTarget).addClass('text-danger');
     })
+    // Esconde y muestra el texto al hacer doble click en el titulo de los cards en la section de recetas
     $(".card-body h5").on("dblclick",() => {
         $("p").toggle();
-
     })
-
+    //smooth-scroll
+    $("a").on('click',(event) => {
+        if (event.currentTarget.hash !== "") {
+            event.preventDefault();
+            let hash = event.currentTarget.hash;
+            $('html,body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, () => {
+                window.location.hash = hash;
+                });
+            }
+        });
 })
